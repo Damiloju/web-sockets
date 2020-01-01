02a394dd61802b020fd7f8d0ee24d597d4ad7af3
Oluwadamiloju <yusufdamiloju@gmail.com>
2020-01-01 19:52:32 +0100

    users can now share location

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,18 @@ form.addEventListener("submit", event => {
   const message = event.target.elements.message.value;
   socket.emit("sendMessage", message);
 });
+
+const locationBtn = document.getElementById("share_location");
+
+locationBtn.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your broswer");
+  }
+
+  navigator.geolocation.getCurrentPosition(position => {
+    socket.emit("shareLocation", {
+      long: position.coords.longitude,
+      lat: position.coords.longitude
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -24,6 +24,13 @@ io.on("connection", socket => {
     io.emit("message", message);
   });
 
+  socket.on("shareLocation", position => {
+    io.emit(
+      "message",
+      `https://google.com/maps?=${position.lat},${position.long}`
+    );
+  });
+
   socket.on("disconnect", () => {
     io.emit("message", "A user has left");
   });
